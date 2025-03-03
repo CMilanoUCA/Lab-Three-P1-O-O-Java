552a277126d7d97a1c40a0878cd83e9d6c47f738
CMilanoUCA <cmilano@cub.uca.edu>
2025-03-03 10:09:47 -0600

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@
 // Lab 3 PT. 1 (Data Display (Console Ver.))
 
 
-
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
